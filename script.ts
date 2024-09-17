416d4f1670307a62144a46d93237a309c2d34c98
Yosef Haim <yosefhaimmalamud@gmail.com>
2024-09-17 18:11:51 +0300

    First commit

---
+++ script.ts
@@ -0,0 +1,10 @@
+const input = document.getElementById('add-input') as HTMLInputElement;
+const button = document.getElementById('add-button') as HTMLButtonElement;
+
+interface Task {
+    id?: 
+}
+
+function createTaskElement(task: Task): HTMLDivElement {
+    
+}
